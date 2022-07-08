48dd5f3bc55c87f2bbec3c23b7cc81ba6b6d0a5f
EmilianoJZX100 <5083@holbertonschool.com>
2022-07-08 12:32:22 -0700

    a

--- print.c
+++ print.c
@@ -15,7 +15,8 @@ int _printf(const char *format, ...)
 		if (format[y + 1] == '\0')
 		{
 			return(-1);
-			if (format[y] == '%' || format[y + 1] == '%')
+
+			if (format[y] == '%' && format[y + 1] == 0 || format[y + 1] == '%')
 		{
 			_putchar('%');
 			y++;
