c53e448d5748cb8cc005031c03733becaf8fa39c
santirodriguezzz <santiagorodri130898@gmail.com>
2022-07-08 09:08:12 -0700

    printf

--- main.h
+++ main.h
@@ -14,8 +14,8 @@ typedef struct structure
 } s_t;
 int _printf(const char *format, ...);
 int _putchar(char c);
-int print_ch(va_list);
-int print_str(va_list);
-int print_per(va_list);
+int print_ch(va_list list);
+int print_str(va_list list);
+int print_per(va_list list);
 int auxs(va_list list, char c);
 #endif
