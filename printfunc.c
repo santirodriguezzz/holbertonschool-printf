8c5e41b37695aa813b5ac5417dee7875e52df2fe
EmilianoJZX100 <5083@holbertonschool.com>
2022-07-10 18:20:08 -0700

    a

--- printfunc.c
+++ printfunc.c
@@ -67,7 +67,7 @@ int print_per(va_list list)
 int print_num(va_list list)
 {
 	int n = va_arg(list, int);
-	int i, count = 0;
+	int i, j, count = 0;
 
 	if (n == 0)
 	{
@@ -76,9 +76,15 @@ int print_num(va_list list)
 	}
 	if (n < 0 || n == INT_MIN)
 	{
-		if (n != INT_MIN)
+		if (n == INT_MIN)
 		{
+			int arr[] = {2,1,4,7,4,8,3,6,4,8};
 			_putchar('-');
+			for (j = 0; j < 10; j++)
+			{
+				_putchar(arr[j]);
+			}
+			return (22);
 		}
 		n = n * -1;
 		count++;
